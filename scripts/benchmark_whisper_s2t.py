8c74ca70da11bb4ac4a405ede09c0217ae7091c6
razvanalex <razvansmadu@gmail.com>
2025-04-23 12:07:49 +0300

    refactor: Format benchmark code

--- scripts/benchmark_whisper_s2t.py
+++ scripts/benchmark_whisper_s2t.py
@@ -1,24 +1,36 @@
 import argparse
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--repo_path', default="", type=str)
-    parser.add_argument('--backend', default="CTranslate2", type=str)
-    parser.add_argument('--batch_size', default=16, type=int)
-    parser.add_argument('--flash_attention', default="no", type=str)
-    parser.add_argument('--eval_mp3', default="no", type=str)
-    parser.add_argument('--eval_multilingual', default="yes", type=str)
+    parser.add_argument("--repo_path", default="", type=str)
+    parser.add_argument("--backend", default="CTranslate2", type=str)
+    parser.add_argument("--batch_size", default=16, type=int)
+    parser.add_argument("--flash_attention", default="no", type=str)
+    parser.add_argument("--eval_mp3", default="no", type=str)
+    parser.add_argument("--eval_multilingual", default="yes", type=str)
     args = parser.parse_args()
     return args
 
-def run(repo_path, backend, flash_attention=False, batch_size=16, eval_mp3=False, eval_multilingual=True):
-    import sys, time, os
+
+def run(
+    repo_path,
+    backend,
+    flash_attention=False,
+    batch_size=16,
+    eval_mp3=False,
+    eval_multilingual=True,
+):
+    import os
+    import sys
+    import time
 
     if len(repo_path):
         sys.path.append(repo_path)
 
-    import whisper_s2t
     import pandas as pd
+
+    import whisper_s2t
 
     if backend.lower() in ["huggingface", "hf"]:
         asr_options = {
@@ -38,69 +50,83 @@ def run(repo_path, backend, flash_attention=False, batch_size=16, eval_mp3=False
     model = whisper_s2t.load_model("large-v3", backend=backend, asr_options=asr_options)
 
     # KINCAID46 WAV >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    data = pd.read_csv(f'{repo_path}/data/KINCAID46/manifest_wav.tsv', sep="\t")
-    files = [f"{repo_path}/{fn}" for fn in data['audio_path']]
-    lang_codes = len(files)*['en']
-    tasks = len(files)*['transcribe']
-    initial_prompts = len(files)*[None]
-    
-    _ = model.transcribe_with_vad(files,
-                                    lang_codes=lang_codes, 
-                                    tasks=tasks, 
-                                    initial_prompts=initial_prompts,
-                                    batch_size=batch_size)
+    data = pd.read_csv(f"{repo_path}/data/KINCAID46/manifest_wav.tsv", sep="\t")
+    files = [f"{repo_path}/{fn}" for fn in data["audio_path"]]
+    lang_codes = len(files) * ["en"]
+    tasks = len(files) * ["transcribe"]
+    initial_prompts = len(files) * [None]
+
+    _ = model.transcribe_with_vad(
+        files,
+        lang_codes=lang_codes,
+        tasks=tasks,
+        initial_prompts=initial_prompts,
+        batch_size=batch_size,
+    )
 
     st = time.time()
-    out = model.transcribe_with_vad(files,
-                                    lang_codes=lang_codes, 
-                                    tasks=tasks, 
-                                    initial_prompts=initial_prompts,
-                                    batch_size=batch_size)
-    time_kincaid46_wav = time.time()-st
-    
-    data['pred_text'] = [" ".join([_['text'] for _ in _transcript]).strip() for _transcript in out]
-    data.to_csv(f"{results_dir}/KINCAID46_WAV.tsv", sep="\t", index=False)
+    out = model.transcribe_with_vad(
+        files,
+        lang_codes=lang_codes,
+        tasks=tasks,
+        initial_prompts=initial_prompts,
+        batch_size=batch_size,
+    )
+    time_kincaid46_wav = time.time() - st
 
+    data["pred_text"] = [
+        " ".join([_["text"] for _ in _transcript]).strip() for _transcript in out
+    ]
+    data.to_csv(f"{results_dir}/KINCAID46_WAV.tsv", sep="\t", index=False)
 
     # KINCAID46 MP3 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     if eval_mp3:
-        data = pd.read_csv(f'{repo_path}/data/KINCAID46/manifest_mp3.tsv', sep="\t")
-        files = [f"{repo_path}/{fn}" for fn in data['audio_path']]
-        lang_codes = len(files)*['en']
-        tasks = len(files)*['transcribe']
-        initial_prompts = len(files)*[None]
+        data = pd.read_csv(f"{repo_path}/data/KINCAID46/manifest_mp3.tsv", sep="\t")
+        files = [f"{repo_path}/{fn}" for fn in data["audio_path"]]
+        lang_codes = len(files) * ["en"]
+        tasks = len(files) * ["transcribe"]
+        initial_prompts = len(files) * [None]
 
         st = time.time()
-        out = model.transcribe_with_vad(files,
-                                        lang_codes=lang_codes, 
-                                        tasks=tasks, 
-                                        initial_prompts=initial_prompts,
-                                        batch_size=batch_size)
-        time_kincaid46_mp3 = time.time()-st
-        
-        data['pred_text'] = [" ".join([_['text'] for _ in _transcript]).strip() for _transcript in out]
+        out = model.transcribe_with_vad(
+            files,
+            lang_codes=lang_codes,
+            tasks=tasks,
+            initial_prompts=initial_prompts,
+            batch_size=batch_size,
+        )
+        time_kincaid46_mp3 = time.time() - st
+
+        data["pred_text"] = [
+            " ".join([_["text"] for _ in _transcript]).strip() for _transcript in out
+        ]
         data.to_csv(f"{results_dir}/KINCAID46_MP3.tsv", sep="\t", index=False)
     else:
         time_kincaid46_mp3 = 0.0
 
-
     # MultiLingualLongform
     if eval_multilingual:
-        data = pd.read_csv(f'{repo_path}/data/MultiLingualLongform/manifest.tsv', sep="\t")
-        files = [f"{repo_path}/{fn}" for fn in data['audio_path']]
-        lang_codes = data['lang_code'].to_list()
-        tasks = len(files)*['transcribe']
-        initial_prompts = len(files)*[None]
+        data = pd.read_csv(
+            f"{repo_path}/data/MultiLingualLongform/manifest.tsv", sep="\t"
+        )
+        files = [f"{repo_path}/{fn}" for fn in data["audio_path"]]
+        lang_codes = data["lang_code"].to_list()
+        tasks = len(files) * ["transcribe"]
+        initial_prompts = len(files) * [None]
 
         st = time.time()
-        out = model.transcribe_with_vad(files,
-                                        lang_codes=lang_codes, 
-                                        tasks=tasks, 
-                                        initial_prompts=initial_prompts,
-                                        batch_size=batch_size)
-        time_multilingual = time.time()-st
-        
-        data['pred_text'] = [" ".join([_['text'] for _ in _transcript]).strip() for _transcript in out]
+        out = model.transcribe_with_vad(
+            files,
+            lang_codes=lang_codes,
+            tasks=tasks,
+            initial_prompts=initial_prompts,
+            batch_size=batch_size,
+        )
+        time_multilingual = time.time() - st
+
+        data["pred_text"] = [
+            " ".join([_["text"] for _ in _transcript]).strip() for _transcript in out
+        ]
         data.to_csv(f"{results_dir}/MultiLingualLongform.tsv", sep="\t", index=False)
     else:
         time_multilingual = 0.0
@@ -109,16 +135,23 @@ def run(repo_path, backend, flash_attention=False, batch_size=16, eval_mp3=False
         ["Dataset", "Time"],
         ["KINCAID46 WAV", time_kincaid46_wav],
         ["KINCAID46 MP3", time_kincaid46_mp3],
-        ["MultiLingualLongform", time_multilingual]
+        ["MultiLingualLongform", time_multilingual],
     ]
     infer_time = pd.DataFrame(infer_time[1:], columns=infer_time[0])
     infer_time.to_csv(f"{results_dir}/infer_time.tsv", sep="\t", index=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_arguments()
     eval_mp3 = True if args.eval_mp3 == "yes" else False
     eval_multilingual = True if args.eval_multilingual == "yes" else False
     flash_attention = True if args.flash_attention == "yes" else False
 
-    run(args.repo_path, args.backend, flash_attention=flash_attention, batch_size=args.batch_size, eval_mp3=eval_mp3, eval_multilingual=eval_multilingual)
+    run(
+        args.repo_path,
+        args.backend,
+        flash_attention=flash_attention,
+        batch_size=args.batch_size,
+        eval_mp3=eval_mp3,
+        eval_multilingual=eval_multilingual,
+    )
--- tools/metrics.py
+++ tools/metrics.py
@@ -1,27 +1,44 @@
-import jiwer
-import editdistance
 import diff_match_patch
-
+import editdistance
+import jiwer
 import numpy as np
-from tqdm import tqdm
-from nltk import ngrams
 from jiwer.transformations import wer_default
+from nltk import ngrams
+from tqdm import tqdm
 
 
 def WER(references, hypotheses):
-    measures = jiwer.compute_measures(references, 
-                                      hypotheses,
-                                      truth_transform=wer_default, 
-                                      hypothesis_transform=wer_default)
-    
+    measures = jiwer.compute_measures(
+        references,
+        hypotheses,
+        truth_transform=wer_default,
+        hypothesis_transform=wer_default,
+    )
+
     out = {
-        'WER': round(100*measures['wer'], 2),
-        'IER': round(100*measures['insertions']/(measures['hits']+measures['substitutions']+measures['deletions']), 2),
-        'DER': round(100*measures['deletions']/(measures['hits']+measures['substitutions']+measures['deletions']), 2),
-        'SER': round(100*measures['substitutions']/(measures['hits']+measures['substitutions']+measures['deletions']), 2)
+        "WER": round(100 * measures["wer"], 2),
+        "IER": round(
+            100
+            * measures["insertions"]
+            / (measures["hits"] + measures["substitutions"] + measures["deletions"]),
+            2,
+        ),
+        "DER": round(
+            100
+            * measures["deletions"]
+            / (measures["hits"] + measures["substitutions"] + measures["deletions"]),
+            2,
+        ),
+        "SER": round(
+            100
+            * measures["substitutions"]
+            / (measures["hits"] + measures["substitutions"] + measures["deletions"]),
+            2,
+        ),
     }
-    
+
     return out
+
 
 def CER(references, hypotheses):
     errors = 0
@@ -33,11 +50,13 @@ def CER(references, hypotheses):
         words += len(r_list)
         errors += editdistance.eval(h_list, r_list)
 
-    return round(100*errors/words, 2)
+    return round(100 * errors / words, 2)
+
 
 def NGramDuplicates(text, ngram_size=5):
     all_ngrams = list(ngrams(text.split(), ngram_size))
-    return len(all_ngrams)-len(set(all_ngrams))
+    return len(all_ngrams) - len(set(all_ngrams))
+
 
 def NGramInsertions(references, hypotheses, ngram_size=5):
 
@@ -61,34 +80,49 @@ def NGramInsertions(references, hypotheses, ngram_size=5):
 
         for k, v in hyp_counts.items():
             if (v > 1) and (ref_counts.get(k, 1) < v):
-                repeated_ngrams += (v-ref_counts.get(k, 1))
+                repeated_ngrams += v - ref_counts.get(k, 1)
 
     return repeated_ngrams
+
 
 def evaluate(references, hypotheses, cer=False, ngram_size=5):
     scores = WER(references, hypotheses)
     if cer:
-        scores.update({'CER': CER(references, hypotheses), f'{ngram_size}-GramInsertions': NGramInsertions(references, hypotheses, ngram_size=ngram_size)})
+        scores.update(
+            {
+                "CER": CER(references, hypotheses),
+                f"{ngram_size}-GramInsertions": NGramInsertions(
+                    references, hypotheses, ngram_size=ngram_size
+                ),
+            }
+        )
     else:
-        scores.update({f'{ngram_size}-GramInsertions': NGramInsertions(references, hypotheses,  ngram_size=ngram_size)})
-        
+        scores.update(
+            {
+                f"{ngram_size}-GramInsertions": NGramInsertions(
+                    references, hypotheses, ngram_size=ngram_size
+                )
+            }
+        )
+
     return scores
+
 
 def word_alignment_accuracy_single(references, hypotheses, collar=0.2):
     # Find diffs between ref and hyp
-    r_list = [_['word'].replace(" ", "_") for _ in references]
-    h_list = [_['word'].replace(" ", "_") for _ in hypotheses]
-    
-    orig_words = '\n'.join(r_list) + '\n'
-    pred_words = '\n'.join(h_list) + '\n'
-    
+    r_list = [_["word"].replace(" ", "_") for _ in references]
+    h_list = [_["word"].replace(" ", "_") for _ in hypotheses]
+
+    orig_words = "\n".join(r_list) + "\n"
+    pred_words = "\n".join(h_list) + "\n"
+
     diff = diff_match_patch.diff_match_patch()
     diff.Diff_Timeout = 0
     orig_enc, pred_enc, enc = diff.diff_linesToChars(orig_words, pred_words)
     diffs = diff.diff_main(orig_enc, pred_enc, False)
     diff.diff_charsToLines(diffs, enc)
-    
-    diffs_post = [(d[0], d[1].replace('\n', ' ').strip().split()) for d in diffs]
+
+    diffs_post = [(d[0], d[1].replace("\n", " ").strip().split()) for d in diffs]
 
     # Find words which got HIT and their matching
     r_idx, h_idx = 0, 0
@@ -104,27 +138,30 @@ def word_alignment_accuracy_single(references, hypotheses, collar=0.2):
                 r_idx += 1
                 h_idx += 1
 
-
     # Find words whose alignments overlap with each other
     overlapped_words = 0
     within_collar_words = 0
     for r_idx, h_idx in word_idx_match.items():
-        if (hypotheses[h_idx]['start']<references[r_idx]['end']) and (hypotheses[h_idx]['end']>references[r_idx]['start']):
+        if (hypotheses[h_idx]["start"] < references[r_idx]["end"]) and (
+            hypotheses[h_idx]["end"] > references[r_idx]["start"]
+        ):
             overlapped_words += 1
 
-        if (hypotheses[h_idx]['start']>=references[r_idx]['start']-collar) and (hypotheses[h_idx]['end']<=references[r_idx]['end']+collar):
+        if (hypotheses[h_idx]["start"] >= references[r_idx]["start"] - collar) and (
+            hypotheses[h_idx]["end"] <= references[r_idx]["end"] + collar
+        ):
             within_collar_words += 1
 
-    
     results = {
-        'acc_overlapped': round(100*overlapped_words/len(word_idx_match), 2),
-        'acc_within_collar': round(100*within_collar_words/len(word_idx_match), 2),
-        'overlapped_words': overlapped_words,
-        'within_collar_words': within_collar_words,
-        'total_hit_words': len(word_idx_match),
+        "acc_overlapped": round(100 * overlapped_words / len(word_idx_match), 2),
+        "acc_within_collar": round(100 * within_collar_words / len(word_idx_match), 2),
+        "overlapped_words": overlapped_words,
+        "within_collar_words": within_collar_words,
+        "total_hit_words": len(word_idx_match),
     }
 
     return results
+
 
 def word_alignment_accuracy(references, hypotheses, collar=0.2):
     overlapped_words = 0
@@ -133,16 +170,16 @@ def word_alignment_accuracy(references, hypotheses, collar=0.2):
 
     for r, h in tqdm(zip(references, hypotheses), total=len(references)):
         res = word_alignment_accuracy_single(r, h, collar=collar)
-        overlapped_words += res['overlapped_words']
-        within_collar_words += res['within_collar_words']
-        total_hit_words += res['total_hit_words']
+        overlapped_words += res["overlapped_words"]
+        within_collar_words += res["within_collar_words"]
+        total_hit_words += res["total_hit_words"]
 
     results = {
-        'acc_overlapped': round(100*overlapped_words/total_hit_words, 2),
-        'acc_within_collar': round(100*within_collar_words/total_hit_words, 2),
-        'overlapped_words': overlapped_words,
-        'within_collar_words': within_collar_words,
-        'total_hit_words': total_hit_words,
+        "acc_overlapped": round(100 * overlapped_words / total_hit_words, 2),
+        "acc_within_collar": round(100 * within_collar_words / total_hit_words, 2),
+        "overlapped_words": overlapped_words,
+        "within_collar_words": within_collar_words,
+        "total_hit_words": total_hit_words,
     }
 
     return results
